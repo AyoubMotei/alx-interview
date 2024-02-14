8ad1ed2a7f72d6e240ee03891bb5f2c34c6c1237
AyoubMotei <ayoub.motei@gmail.com>
2024-02-14 22:45:53 +0100

    Update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,27 +1,28 @@
 #!/usr/bin/node
-
-const request = require("request");
-
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
-
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+const movieId = process.argv[2];
+request(url + movieId, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printInOrder(characters);
   }
+}
 );
+
+async function printInOrder (characters) {
+  for (const character of characters) {
+    await new Promise((resolve, reject) => {
+      request(character, function (error, response, body) {
+        if (error) {
+          reject(error);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
+    });
+  }
+}
